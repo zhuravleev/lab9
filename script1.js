a536c6c5ca29533e91edd6a1c633a9a3a828075e
Zerstörer <yuriyzhuravlev171@gmail.com>
2020-12-22 01:36:35 +0700

    First

---
+++ script1.js
@@ -0,0 +1,9 @@
+console.log("Задание №1");
+
+let phone = {
+    name: "Samsung", 
+    color: "Белый", 
+    cost: "5000", 
+}; 
+
+console.log(phone.color + " телефон фирмы " + phone.name + " стоит " + phone.cost + " рублей." + ""); 
